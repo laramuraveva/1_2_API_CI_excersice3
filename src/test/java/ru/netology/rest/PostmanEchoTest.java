131488f720a633b826e09d8a4dd365faea7012ac
laramuraveva <lara.chernishova@yandex.ru>
2022-08-05 10:24:35 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void testSomeData() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"))
+        ;
+    }
+}
